702091027cb89fc91010484bf645a8c5c742f35b
John Crawford <coffeeboi47@gmail.com>
2022-08-02 22:53:48 -0600

    if __name__ == '__main__': main()

--- src/isthatme.py
+++ src/isthatme.py
@@ -6,27 +6,32 @@ import os
 import pwd
 import sys
 
-file = sys.argv[0]
-help_message = f"""Usage: {file} [OPTION]...
+program = sys.argv[0]
+help_message = f"""Usage: {program} [OPTION]...
 Print the user name associated with the current effective user ID.
 Same as id -un.
 
       --help        display this help and exit
       --version     output version information and exit\n"""
-version_message = f"""isthatme (pythonutils) 2022.07.29
+version_message = f"""isthatme (pyutils) 2022.08.02
 Written by John Crawford"""
 
-for x in sys.argv:
-    match x:
-        case "--help":
-            print(help_message)
-            exit(0)
-        case "--version":
-            print(version_message)
-            exit(0)
+def get_args(): #parse arguments
+    for x in sys.argv:
+        match x:
+            case "--help":
+                print(help_message)
+                exit(0)
+            case "--version":
+                print(version_message)
+                exit(0)
 
-try:
-    print(pwd.getpwuid(os.getuid())[0])
-except:
-    print(f"cannot find name for user ID {os.getuid()}")
+def main():
+    get_args()
+    try:
+        print(pwd.getpwuid(os.getuid())[0])
+    except:
+        print(f"cannot find name for user ID {os.getuid()}")
 
+if __name__ == "__main__":
+    main()
