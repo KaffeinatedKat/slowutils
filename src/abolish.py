e729f10485896f5b1421dbf3c4a9512cae3cbc1e
John Crawford <coffeeboi47@gmail.com>
2022-09-06 15:34:37 -0600

    undo last merge, did weird stuff

--- src/abolish.py
+++ src/abolish.py
@@ -22,25 +22,17 @@ class Variables:
 
     help_message = f"""Usage: {program} [OPTION]... [FILE]...
 Remove (unlink) the FILE(s).
-
-<<<<<<< Updated upstream
   -i                    prompt before every removal
-=======
->>>>>>> Stashed changes
   -f, --force           ignore nonexistent files and arguments, never prompt
   -r, -R, --recursive   remove directories and their contents recursively
   -d, --dir             remove empty directories
       --help        display this help and exit
       --version     output version information and exit
-
 By default, rm does not remove directories.  Use the --recursive (-r or -R)
 option to remove each listed directory, too, along with all of its contents.
-
 To remove a file whose name starts with a '-', for example '-foo',
 use this command:
-
   {program} ./-foo
-
 Note that if you use abolish to remove a file, it might be possible to recover
 some of its contents, given sufficient expertise and/or time.  For greater
 assurance that the contents are truly unrecoverable, consider using shred(1)."""
@@ -70,7 +62,6 @@ class Path:
         self.is_file = False
         self.is_folder = False
         self.is_symlink = False
-<<<<<<< Updated upstream
         self.folders = []
 
     def get_type(self):
@@ -150,67 +141,6 @@ class Path:
         if Vars.verbose and Vars.success: # print verbose message if file deletion successful
            print(f"{Vars.verbose_message} '{self.path}'")
 
-=======
-
-
-    def get_type(self):
-        self.is_file = not os.path.isdir(self.path)
-        self.is_folder = os.path.isdir(self.path)
-        self.is_symlink = os.path.islink(self.path)
-
-
-    def ask_delete(self, Vars, Error):
-        if self.is_symlink:
-            if stdin(f"{Vars.program}: remove symbolic link '{self.path}'? "):
-                self.delete(Vars, Error)
-        
-        elif self.is_folder:
-            if len(os.listdir(self.path)) == 0 and Vars.delete_empty: # if --dir and folder is empty
-                if stdin(f"{Vars.program}: remove directory '{self.path}'? ").lower().startswith("y"):
-                    self.delete(Vars, Error)
-                return
-            if Vars.recursive:
-                print("sucks to suck, directory has stuff in it lmao")
-            else:
-                Error.IsAFolder(self)
-
-        elif not self.is_folder:
-            if stdin(f"{Vars.program}: remove regular file '{self.path}'? ").lower().startswith("y"):
-                self.delete(Vars, Error)
-
-
-    def delete(self, Vars, Error):
-        if self.is_symlink:
-            Vars.verbose_message = "removed"
-            os.unlink(self.path)
-
-        elif self.is_file: # files
-            Vars.verbose_message = "removed"
-            try:
-                os.remove(self.path)
-                Vars.success = True
-            except FileNotFoundError:
-                if not Vars.force:
-                    Error.FileNotFound(self)
-
-        elif self.is_folder: # directories
-            Vars.verbose_message = "removed directory"
-    
-            if Vars.recursive: # delete full directories
-                shutil.rmtree(self.path)
-                Vars.success = True
-
-            elif Vars.delete_empty: #remove empty directories
-                try:
-                    os.rmdir(self.path)
-                    Vars.success = True
-                except OSError:
-                    Error.FolderNotEmpty(self) # -f does not ignore "Directory not empty" error
-
-            elif not Vars.recursive: # -r nor -d were set
-                Error.IsAFolder(self)
-
->>>>>>> Stashed changes
 
 
 def stdin(text):
@@ -237,11 +167,7 @@ def get_args(Vars, File, Error):
             if "--help" in x:
                 print(Vars.help_message)
                 exit(0)
-<<<<<<< Updated upstream
             if "-r" in x.lower() or "--recursive" in x: #recursive '-r'
-=======
-            if "-r" in x.lower() or "--recursive" in x:
->>>>>>> Stashed changes
                 Vars.recursive = True
             if "-f" in x.lower() or "--force" in x:
                 Vars.force = True
@@ -270,7 +196,6 @@ def main():
         Vars.success = False
         File.path = os.path.join(x)
         File.get_type()
-<<<<<<< Updated upstream
         
         
         if Vars.annoy:
@@ -279,17 +204,6 @@ def main():
             File.delete(Vars, Error)
         
         
-=======
-        
-        if Vars.annoy:
-            File.ask_delete(Vars, Error)
-        elif not Vars.annoy:
-            File.delete(Vars, Error)
-        
-        if Vars.verbose and Vars.success: # print verbose message if file deletion successful
-           print(f"{Vars.verbose_message} '{File.path}'")
-
->>>>>>> Stashed changes
 
 if __name__ == '__main__':
     main()
